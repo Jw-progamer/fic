b0937143766a173e77324bee4c2f6c44ed3681d2
Josias Wando <josiasn.queiroz@gmail.com>
2018-10-05 10:38:26 -0300

    Uma correcao pontual no projeto

--- fics/capitulo/serializers.py
+++ fics/capitulo/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Capitulo
 
-class CapituloSerializer(serializers.Serializer):
+class CapituloSerializer(serializers.ModelSerializer):
     class Meta:
         model = Capitulo
         fields = ('id','indice','titulo','texto','historia')
